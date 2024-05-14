d8a0a0dd26838f3a054a2de629eed03f55068f26
Rick van der Sommen <rickvdsommen@hotmail.com>
2024-05-14 15:11:22 +0200

    Delete welcome blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -40,15 +40,6 @@
                                     >
                                         Log in
                                     </a>
-
-                                    @if (Route::has('register'))
-                                        <a
-                                            href="{{ route('register') }}"
-                                            class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-                                        >
-                                            Register
-                                        </a>
-                                    @endif
                                 @endauth
                             </nav>
                         @endif
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,11 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [AuthenticatedSessionController::class, 'create'])
+    ->name('login');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
