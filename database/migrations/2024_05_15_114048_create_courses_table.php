6acd3772cfe35d632afc114f9b8c8337fa7cb1f4
Reigen073 <129341199+Reigen073@users.noreply.github.com>
2024-05-17 00:42:07 +0200

    camelCase fix
    coursename into courseName

--- database/migrations/2024_05_15_114048_create_courses_table.php
+++ database/migrations/2024_05_15_114048_create_courses_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->timestamps();
             $table->string('email');
-            $table->string('coursename');
+            $table->string('courseName');
             $table->string('description')->nullable();
 
 
