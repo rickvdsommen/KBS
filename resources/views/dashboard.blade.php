84b09394f4c13e694a826d9f92eb5d88742e6344
Rick van der Sommen <rickvdsommen@hotmail.com>
2024-05-22 10:46:15 +0200

    Added KBS favicon

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,8 +7,7 @@
 
     <div class="pb-12 pt-5">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-700 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
+            <div class="p-4 sm:p-8 text-gray-900 dark:text-gray-100 bg-white dark:bg-gray-700 shadow sm:rounded-lg">
                     {{ __("Je bent ingelogd!") }} <br/><br/>
 
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat illo voluptas expedita, quae, optio nisi tenetur assumenda numquam perspiciatis quod fugiat ex, qui recusandae. Incidunt aut nulla doloremque temporibus ducimus!</p>
