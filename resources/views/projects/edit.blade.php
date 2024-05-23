b851c48af77f9f80d89e082a164bcea60f869d94
Reigen073 <129341199+Reigen073@users.noreply.github.com>
2024-05-23 13:53:54 +0200

    Update fix
    Update werkt maar moet nog aanpassingen komen

--- resources/views/projects/edit.blade.php
+++ resources/views/projects/edit.blade.php
@@ -1,6 +1,4 @@
-@extends('layouts.app')
 
-@section('content')
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
@@ -57,4 +55,3 @@
     </div>
 </x-app-layout>
 
-@endsection
