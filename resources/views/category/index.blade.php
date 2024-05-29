efa67bf3f381feb60d7134e4a176b02cb66ec328
Rick van der Sommen <rickvdsommen@hotmail.com>
2024-05-29 16:40:20 +0200

    css tags en categories

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -10,49 +10,56 @@
                 <h1 class="text-2xl mb-6">Categorie Toevoegen</h1>
                 <form action="{{ route('categories.store') }}" method="POST" class="flex">
                     @csrf
-                    <input id="category" placeholder="Categorie" class="mb-2 w-80 rounded-md border-gray-300 dark:border-gray-700 shadow-sm" type="text" name="category" :value="old('category')" required autofocus />
+                    <input id="category" placeholder="Categorie"
+                        class="mb-2 w-80 rounded-md border-gray-300 dark:border-gray-700 shadow-sm" type="text"
+                        name="category" :value="old('category')" required autofocus />
                     <x-primary-button class="w-30 h-8 mt-1 ml-2">{{ __('Toevoegen') }}</x-primary-button>
                 </form>
             </div>
         </div>
     </div>
-    <div class="">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg p-6">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg p-6">
 
-                <div class="mb-2">
-                    <form action="{{ route('categories.index') }}" method="GET" class="flex">
-                        <input id="search" placeholder="Zoek categories..." class="mb-2 w-80 rounded-md border-gray-300 dark:border-gray-700 shadow-sm" type="text" name="search" value="{{ request('search') }}" autofocus />
-                        <x-primary-button class="w-30 h-8 mt-1 ml-2">Zoeken</x-primary-button>
-                    </form>
-                </div>
+            <div class="mb-2">
+                <form action="{{ route('categories.index') }}" method="GET" class="flex">
+                    <input id="search" placeholder="Zoek categories..."
+                        class="mb-2 w-80 rounded-md border-gray-300 dark:border-gray-700 shadow-sm" type="text"
+                        name="search" value="{{ request('search') }}" autofocus />
+                    <x-primary-button class="w-30 h-8 mt-1 ml-2">Zoeken</x-primary-button>
+                </form>
+            </div>
 
-                @if ($categories->isEmpty())
-                    <p class="text-gray-700 dark:text-gray-300">Kon geen categorieën vinden.</p>
-                @else
-                    <ul class="space-y-4">
-                        @foreach($categories as $category)
-                            <li class="flex justify-between items-center p-4 bg-gray-100 dark:bg-gray-700 rounded-lg shadow">
-                                <span>{{ $category->category }}</span>
-                                <div class="flex space-x-2">
-                                    <a href="{{ route('categories.edit', $category->id) }}" class="text-blue-600 hover:text-blue-900">
-                                        <x-primary-button>Wijzigen</x-primary-button>
-                                    </a>
-                                    <form action="{{ route('categories.destroy', $category->id) }}" method="POST" onsubmit="return confirm('Are you sure you want to delete this category?');">
-                                        @csrf
-                                        @method('DELETE')
-                                        <x-secondary-button type="submit" class="text-red-600 hover:text-red-900">Verwijderen</x-secondary-button>
-                                    </form>
-                                </div>
-                            </li>
-                        @endforeach
-                    </ul>
-                @endif
-            </div>
-            <!-- Pagination links -->
-            <div class="mt-4">
-                {{ $categories->links() }}
-            </div>
+            @if ($categories->isEmpty())
+                <p class="text-gray-700 dark:text-gray-300">Kon geen categorieën vinden.</p>
+            @else
+                <ul class="space-y-4">
+                    @foreach ($categories as $category)
+                        <li
+                            class="flex items-center p-4 bg-gray-100 dark:bg-gray-700 rounded-lg shadow min-w-fit w-1/3">
+                            <span>{{ $category->category }}</span>
+                            <div class=" flex space-x-2 ml-auto pl-2">
+                                <a href="{{ route('categories.edit', $category->id) }}"
+                                    class="text-blue-600 hover:text-blue-900">
+                                    <x-primary-button>Wijzigen</x-primary-button>
+                                </a>
+                                <form action="{{ route('categories.destroy', $category->id) }}" method="POST"
+                                    onsubmit="return confirm('Weet je zeker dat je deze categorie wilt verwijderen?');">
+                                    @csrf
+                                    @method('DELETE')
+                                    <x-secondary-button type="submit"
+                                        class="text-red-600 hover:text-red-900">Verwijderen</x-secondary-button>
+                                </form>
+                            </div>
+                        </li>
+                    @endforeach
+                </ul>
+            @endif
         </div>
+        <!-- Pagination links -->
+        <div class="mt-4">
+            {{ $categories->links() }}
+        </div>
+
     </div>
 </x-app-layout>
