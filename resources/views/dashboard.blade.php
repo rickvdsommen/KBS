d3e415df257b191270bd9e87754a1d232372c5f4
Reigen073 <129341199+Reigen073@users.noreply.github.com>
2024-06-06 15:04:59 +0200

    Dashboard gemaakt
    Dashboard gemaakt met

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,17 +8,49 @@
     <div class="pb-12 pt-5">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="p-4 sm:p-8 text-gray-900 dark:text-gray-100 bg-white dark:bg-gray-700 shadow sm:rounded-lg">
-                    {{ __("Je bent ingelogd!") }} <br/><br/>
 
-                    <h1>Welkom bij de K.B.S.</h1>
-                    <p>Je kan hier veel bekijken zoals lopende projecten, onze team en je eigen agenda.</p>        
-                    @role('admin')
-                        <br><p>Je bent ingelogd als een ADMIN!</p>
-                    @else
-                        <br><p>Je bent ingelogd als een GEBRUIKER!</p>
-                    @endrole
+                <div class="flex">
+                    <div class="w-1/2">
+                        <h2 class="mt-8 text-xl font-semibold">Agenda</h2>
+                        <div class="min-h-screen " id="calendarDash"></div>
+                    </div>
+                    <div class="w-1/2 pl-4">
+                        <h2 class="mt-8 text-xl font-semibold">Medewerkers aanwezig vandaag</h2>
+                        <ul class="mt-14 divide-y divide-gray-200 dark:divide-gray-600 " >
+                            @forelse ($updatedDevices as $device)
+                                <li class="py-2 flex items-center justify-between">
+                                    <div>
+                                        <a href="{{ route('team.show', $device->user->id) }}" class="text-blue-500 hover:text-blue-700 dark:text-blue-400 dark:hover:text-blue-600">
+                                            {{ $device->user->name }}
+                                        </a>
+                                        <span class="text-gray-500 dark:text-gray-400 text-sm ml-2">
+                                            @if ($device->status === 'aanwezig')
+                                                Aanwezig
+                                            @else
+                                                Bezet
+                                            @endif
+                                        </span>
+                                    </div>
+                                    <!-- Add any additional information or actions for each user here -->
+                                </li>
+                            @empty
+                                <li class="py-2">Er zijn geen medewerkers aanwezig vandaag.</li>
+                            @endforelse
+                        </ul>
 
-                    <br><div class="min-h-screen w-80" id="calendarDash"></div>
+                        <h2 class="mt-8 text-xl font-semibold">Mijn Projecten</h2>
+                        <ul class="mt-4 divide-y divide-gray-200 dark:divide-gray-600">
+                            @forelse ($userProjects as $project)
+                                <li class="py-2">
+                                    <a href="{{ route('projects.show', $project->id) }}" class="text-blue-500 hover:text-blue-700 dark:text-blue-400 dark:hover:text-blue-600">
+                                        {{ $project->projectname }}
+                                    </a>
+                                </li>
+                            @empty
+                                <li class="py-2">Je hebt geen lopende projecten.</li>
+                            @endforelse
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
