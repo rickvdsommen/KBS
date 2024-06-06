3347fcbd3a3efde0b49a73208fbb1b62068d289d
Rick van der Sommen <rickvdsommen@hotmail.com>
2024-06-06 18:28:39 +0200

    Update 2024_05_15_122944_create_devices_table.php

--- database/migrations/2024_05_15_122944_create_devices_table.php
+++ database/migrations/2024_05_15_122944_create_devices_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('devices', function (Blueprint $table) {
             $table->id();
             $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
-            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP'));
             $table->string('status')->default('Afwezig');
             $table->unsignedBigInteger('user_id');
             
