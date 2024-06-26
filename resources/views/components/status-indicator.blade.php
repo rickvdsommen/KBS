2ef29471a2974cf3ef600e26840d37210bb9efd3
Rick van der Sommen <rickvdsommen@hotmail.com>
2024-06-26 11:30:38 +0200

    status indicator centered

--- resources/views/components/status-indicator.blade.php
+++ resources/views/components/status-indicator.blade.php
@@ -1,15 +1,15 @@
 @if($user->device && $user->device->status == 'aanwezig' && $user->device->updated_at->isToday())
-    <span class="inline-flex items-center">
-        <span class="w-2 h-2 me-1 bg-green-500 rounded-full"></span>
+    <span class="inline-flex items-center h-5">
+        <span class="w-2 h-2 mr-1 bg-green-500 rounded-full"></span>
     </span>
 @else
     @if($user->device && $user->device->status == 'bezet' && $user->device->updated_at->isToday())
-        <span class="inline-flex items-center">
-            <span class="w-2 h-2 me-1 bg-red-500 rounded-full"></span>
+        <span class="inline-flex items-center h-5">
+            <span class="w-2 h-2 mr-1 bg-red-500 rounded-full"></span>
         </span>
     @else
-        <span class="inline-flex items-center">
-            <span class="w-2 h-2 me-1 bg-gray-500 rounded-full"></span>
+        <span class="inline-flex items-center h-5">
+            <span class="w-2 h-2 mr-1 bg-gray-500 rounded-full"></span>
         </span>
     @endif
 @endif
