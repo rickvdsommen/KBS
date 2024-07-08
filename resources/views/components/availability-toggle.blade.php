f2a249a5c8ef65fd11303f86f78cff05907d53bc
Rick van der Sommen <rickvdsommen@hotmail.com>
2024-07-08 09:09:04 +0200

    added more checks

--- resources/views/components/availability-toggle.blade.php
+++ resources/views/components/availability-toggle.blade.php
@@ -2,19 +2,22 @@
     @csrf
     <div class="flex flex-col sm:flex-row w-full">
         <label for="available" class="cursor-pointer flex-1">
-            <input type="radio" name="availability" id="available" class="peer sr-only" value="aanwezig" {{ Auth::user()->device->status === 'aanwezig' && Auth::user()->device->updated_at->isToday() ? 'checked' : '' }} />
-            <span class="relative inline-flex h-9 items-center space-x-2 py-2 pl-7 pr-3 text-sm peer-checked:bg-green-200 rounded-md w-60 sm:w-full">
+            <input type="radio" name="availability" id="available" class="peer sr-only" value="aanwezig" 
+                {{ (Auth::user()->device && Auth::user()->device->status === 'aanwezig' && Auth::user()->device->updated_at && Auth::user()->device->updated_at->isToday()) ? 'checked' : '' }} />
+            <span class="relative inline-flex h-9 items-center space-x-2 py-2 pl-7 pr-3 text-sm peer-checked:bg-green-200 rounded-lg w-60 sm:w-full">
                 <span class="before:absolute before:left-3 before:top-[14px] before:h-2 before:w-2 before:rounded-full before:bg-green-500 w-full">Aanwezig</span>
             </span>
         </label>
         <label for="busy" class="cursor-pointer flex-1">
-            <input type="radio" name="availability" id="busy" class="peer sr-only" value="bezet" {{ Auth::user()->device->status === 'bezet' && Auth::user()->device->updated_at->isToday() ? 'checked' : '' }} />
+            <input type="radio" name="availability" id="busy" class="peer sr-only" value="bezet" 
+                {{ (Auth::user()->device && Auth::user()->device->status === 'bezet' && Auth::user()->device->updated_at && Auth::user()->device->updated_at->isToday()) ? 'checked' : '' }} />
             <span class="relative inline-flex h-9 items-center space-x-2 py-2 pl-7 pr-3 text-sm peer-checked:bg-red-200 rounded-lg w-60 sm:w-full">
                 <span class="before:absolute before:left-3 before:top-[14px] before:h-2 before:w-2 before:rounded-full before:bg-red-500">Bezet</span>
             </span>
         </label>
         <label for="away" class="cursor-pointer flex-1">
-            <input type="radio" name="availability" id="away" class="peer sr-only" value="afwezig" {{ Auth::user()->device->status === 'afwezig' || !Auth::user()->device->updated_at->isToday() ? 'checked' : '' }} />
+            <input type="radio" name="availability" id="away" class="peer sr-only" value="afwezig" 
+                {{ (Auth::user()->device && (Auth::user()->device->status === 'afwezig' || !Auth::user()->device->updated_at || !Auth::user()->device->updated_at->isToday())) ? 'checked' : '' }} />
             <span class="relative inline-flex h-9 items-center space-x-2 py-2 pl-7 pr-3 text-sm peer-checked:bg-gray-200 rounded-lg w-60 sm:w-full">
                 <span class="before:absolute before:left-3 before:top-[14px] before:h-2 before:w-2 before:rounded-full before:bg-gray-500 w-full">Afwezig</span>
             </span>
