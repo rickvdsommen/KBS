649d480c4d7c55a17f8be56f936ceacdda454f79
Rick van der Sommen <rickvdsommen@hotmail.com>
2024-07-29 15:40:00 +0200

    Deleted unused code

--- app/Http/Controllers/PoiController.php
+++ app/Http/Controllers/PoiController.php
@@ -4,39 +4,11 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use App\Models\User;
-use App\Models\Availability;
-use App\Models\Appointment;
-use App\Models\Project;
-use Illuminate\Support\Carbon;
-
 class PoiController extends Controller
 {
     public function index()
     {
-        try {
-            // Fetch users with today's status updates
-            $users = User::with('availability')
-                ->whereHas('availability', function($query) {
-                    $query->whereDate('updated_at', Carbon::today())
-                        ->whereIn('status', ['aanwezig', 'bezet']);
-                })
-                ->get();
-    
-            // Get the ongoing projects
-            $projects = Project::where('status', 'Lopend')->get();
-    
-            // Fetch today's agenda
-            $appointments = Appointment::whereDate('start', Carbon::today())
-                ->get();
-    
-            return view('poi.index', compact('projects', 'users', 'appointments'));
-        } catch (\Exception $e) {
-            // Log the error or handle it appropriately
-            return back()->withError('Failed to load dashboard data.');
-        }
+            return view('poi.index');
     }
 }    
 
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,5 +1,4 @@
 <x-app-layout>
-
     <!-- Page Content -->
     <div class="mt-8 max-w-7xl mx-auto bg-white dark:bg-gray-700 shadow sm:rounded-lg">
         <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
